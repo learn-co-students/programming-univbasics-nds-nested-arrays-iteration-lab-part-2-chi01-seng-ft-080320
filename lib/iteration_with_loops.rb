07fba0da8ac8c91915a26706fb9fe5272aeff5ce
Lindsay Mecher <lindsaymecher@gmail.com>
2021-01-10 21:59:45 -0600

    reworked

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,21 +1,22 @@
-
+require "pry"
 def find_min_in_nested_arrays(src)
-  counter = 0
-  min_arr = []
-  while counter < src.length do
-    min_value = nil
-    inner_counter = 0
-    while inner_counter < src[counter].length do
-      current_val = src[counter][inner_counter]
-      if min_value == nil 
-        min_value = current_val
-      elsif current_val < min_value
-        min_value = current_val
+  level_one_counter = 0
+  new_arr = []
+  while level_one_counter < src.length
+    level_two_counter = 0
+    min_val = nil
+    while level_two_counter < src[level_one_counter].length
+      current_num = src[level_one_counter][level_two_counter]
+      # if current num is nil or < min_val
+      # set the min_val = current_num
+      if min_val == nil || current_num < min_val
+        min_val = current_num
       end 
-      inner_counter += 1
-    end
-    min_arr << min_value
-    counter += 1
-  end
-  min_arr
+     
+      level_two_counter += 1
+    end 
+    new_arr << min_val
+    level_one_counter += 1
+  end 
+  new_arr
 end
